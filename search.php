d248899540c103c789e4a3ecf352b8ff14302b30
chrstphrdlz <chrstphrdlz@vm-0.chrstphrdlz.koding.kd.io>
2014-03-07 00:52:41 +0000

    Adding html and php to add person

--- search.php
+++ search.php
@@ -1,71 +1,7 @@
 <html>
     <body>
         <?php
-        
-            class PersonOrdering
-            {
-                public $id;
-                public $firstName;
-                public $lastName;
-                public $email;
-            }
-        
-            class database_acessor
-            {
-                private $username = "root";
-                private $password = "password";
-                private $db_name = "Christmas";
-                private $name_search_string = "SELECT * FROM PersonOrdering p WHERE p.lastName LIKE ?";
-                private $hostname;
-                private $my_sql_connection;
-                private $prepared_statement;
-                
-                public function __construct()
-                {
-                    $this->hostname = gethostname();
-                }
-                
-                private function make_statement($statement_string, $params)
-                {
-                    $connecting_name = 'mysql:host='. $this->hostname . ';dbname=' . $this->db_name;
-                    
-                    try
-                    {
-                        $my_sql_connection = new PDO($connecting_name,$this->username,$this->password); 
-                    }
-                    catch(PDOException $e)
-                    {
-                        echo "failed ";
-                        echo $e->getMessage();
-                    }
-                    
-                    if($my_sql_connection->connect_error)
-                    {
-                        die('Could not connect: ' . $my_sql_connection->connect_error);
-                    }
-                    
-                    $prepared_statement = $my_sql_connection->prepare($statement_string, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-                    
-                    $prepared_statement->execute($params);
-                    
-                    $result = $prepared_statement->fetchAll(PDO::FETCH_CLASS, "PersonOrdering");
-                    
-                    return $result;
-                }
-                
-                private function end_statement()
-                {
-                    $this->my_sql_connection = null;
-                }
-                
-                public function search_for_name($name_to_search_for)
-                {
-                    $statement_string = $this->name_search_string;
-                    $returner = $this->make_statement($statement_string, array($name_to_search_for));
-                    $this->end_statement();
-                    return $returner;
-                } 
-            }
+            require 'globalClasses.php';
             $dba = new database_acessor();
             $wordToSearchFor = $_GET["searchBox"];
             $result = $dba->search_for_name($wordToSearchFor);
@@ -93,6 +29,7 @@
             {
                 echo "no results found for " . $wordToSearchFor;
             }
+            echo "<a href='search.html'>Go Back</a>";
         ?>
     </body>
 </html>
