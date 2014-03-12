29befbce062e654eee3237d2383cccad9fe9ec69
chrstphrdlz <chrstphrdlz@vm-0.chrstphrdlz.koding.kd.io>
2014-03-12 22:57:55 +0000

    Added files back

--- addToyOrClothingOrder.php
+++ addToyOrClothingOrder.php
@@ -1,2 +1,11 @@
-<head>
-</head>
+<html>
+    <head>
+    </head>
+    <body>
+        <div id="deliveryRequestDiv" name="deliveryRequestDiv">
+            Delivery (special request only)<br>
+            <input type="radio" name="deliveryRequest" value="Yes">Yes<br>
+            <input type="radio" name="deliveryRequest" value="No">No<br>
+        </div>
+    </body>
+</html>
