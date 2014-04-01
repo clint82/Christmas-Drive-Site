b625103f31ed2ced5ed74538ab2d7edf0b7a5419
chrstphrdlz <chrstphrdlz@vm-0.chrstphrdlz.koding.kd.io>
2014-04-01 21:43:18 +0000

    added

---
+++ charity/includes/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+//define constants here
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'clint_c');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'membership');
+define('ADMIN_KEY', 'ad80857$C');
+define('VOL_KEY', 'vol2357V');
+
+?>
